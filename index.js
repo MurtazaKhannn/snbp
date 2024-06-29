028986b90164592ead967fb3de4b7dbbc428c2bd
MurtazaKhannn <152141459+MurtazaKhannn@users.noreply.github.com>
2024-06-30 01:53:21 +0530

    commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/" , (req , res) => {
 })
 // Image  Storage Engine
 const storage = multer.diskStorage({
-    destination: '/tmp/' ,
+    destination: './upload/images' ,
     filename: (req , file , cb) => {
         return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
