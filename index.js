a1aaa7156d8cfece1f418457c773d7bf8f80cbc5
MurtazaKhannn <152141459+MurtazaKhannn@users.noreply.github.com>
2024-06-30 23:58:25 +0530

    009

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const upload = multer({
     storage: storage
 })
 //Creating Upload Endpoint for images
-app.use('/images' , express.static(path.join(__dirname ,'upload')))
+app.use('/images' , express.static(path.join(__dirname ,'upload/images')))
 app.post("/upload/images" , upload.single('product') , (req , res) => {
     console.log(req.file)
     res.json({
